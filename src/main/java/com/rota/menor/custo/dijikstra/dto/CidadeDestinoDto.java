7af26751d7ffd122536dfabc1cc52a98ac438a52
Teixeira <odair.f.teixeira@gmail.com>
2023-11-27 22:11:37 -0300

    adicionando tempo medio de todas as categorias

--- src/main/java/com/rota/menor/custo/dijikstra/dto/CidadeDestinoDto.java
+++ src/main/java/com/rota/menor/custo/dijikstra/dto/CidadeDestinoDto.java
@@ -20,28 +20,50 @@ public class CidadeDestinoDto {
 
     private BigDecimal distancia;
 
+    private BigDecimal tempoMedioCarro;
+
+    private BigDecimal tempoMedioMoto;
+
+    private BigDecimal tempoMedioOnibus;
+
+    private BigDecimal tempoMedioCaminhao;
+
+    public CidadeDestinoDto(String origem, String destino, BigDecimal distancia) {
+        this.origem = origem;
+        this.destino = destino;
+        this.distancia = distancia;
+        this.tempoMedioCarro = calculaTempoMedioCarro();
+        this.tempoMedioMoto = calculaTempoMedioMoto();
+        this.tempoMedioOnibus = calculaTempoMedioOnibus();
+        this.tempoMedioCaminhao = calculaTempoMedioCaminhao();
+    }
+
     public CidadeDestinoDto(CidadeDestinoEntity cidadeDestinoEntity) {
         this.origem = cidadeDestinoEntity.getOrigem();
         this.destino = cidadeDestinoEntity.getDestino();
         this.distancia = cidadeDestinoEntity.getDistancia();
+        this.tempoMedioCarro = calculaTempoMedioCarro();
+        this.tempoMedioMoto = calculaTempoMedioMoto();
+        this.tempoMedioOnibus = calculaTempoMedioOnibus();
+        this.tempoMedioCaminhao = calculaTempoMedioCaminhao();
     }
 
-    public BigDecimal getTempoMedioCarro() {
+    public BigDecimal calculaTempoMedioCarro() {
         return Objects.nonNull(distancia) ?
                 distancia.divide(BigDecimal.valueOf(100), 2, RoundingMode.HALF_UP) : BigDecimal.ZERO;
     }
 
-    public BigDecimal getTempoMedioMoto() {
+    public BigDecimal calculaTempoMedioMoto() {
         return Objects.nonNull(distancia) ?
                 distancia.divide(BigDecimal.valueOf(120), 2, RoundingMode.HALF_UP) : BigDecimal.ZERO;
     }
 
-    public BigDecimal getTempoMedioOnibus() {
+    public BigDecimal calculaTempoMedioOnibus() {
         return Objects.nonNull(distancia) ?
                 distancia.divide(BigDecimal.valueOf(80), 2, RoundingMode.HALF_UP) : BigDecimal.ZERO;
     }
 
-    public BigDecimal getTempoMedioCaminhao() {
+    public BigDecimal calculaTempoMedioCaminhao() {
         return Objects.nonNull(distancia) ?
                 distancia.divide(BigDecimal.valueOf(60), 2, RoundingMode.HALF_UP) : BigDecimal.ZERO;
     }
--- src/main/java/com/rota/menor/custo/dijikstra/dto/ResultadoDto.java
+++ src/main/java/com/rota/menor/custo/dijikstra/dto/ResultadoDto.java
@@ -15,24 +15,42 @@ import java.util.Objects;
 public class ResultadoDto {
 
     private BigDecimal distanciaTotal;
+
     private List<CidadeDestinoDto> caminho;
 
-    public BigDecimal getTempoMedioCarro() {
+    private BigDecimal tempoMedioCarro;
+
+    private BigDecimal tempoMedioMoto;
+
+    private BigDecimal tempoMedioOnibus;
+
+    private BigDecimal tempoMedioCaminhao;
+
+    public ResultadoDto(BigDecimal distanciaTotal, List<CidadeDestinoDto> caminho) {
+        this.distanciaTotal = distanciaTotal;
+        this.caminho = caminho;
+        this.tempoMedioCarro = calculaTempoMedioCarro();
+        this.tempoMedioMoto = calculaTempoMedioMoto();
+        this.tempoMedioOnibus = calculaTempoMedioOnibus();
+        this.tempoMedioCaminhao = calculaTempoMedioCaminhao();
+    }
+
+    public BigDecimal calculaTempoMedioCarro() {
         return Objects.nonNull(distanciaTotal) ?
                 distanciaTotal.divide(BigDecimal.valueOf(100), 2, RoundingMode.HALF_UP) : BigDecimal.ZERO;
     }
 
-    public BigDecimal getTempoMedioMoto() {
+    public BigDecimal calculaTempoMedioMoto() {
         return Objects.nonNull(distanciaTotal) ?
                 distanciaTotal.divide(BigDecimal.valueOf(120), 2, RoundingMode.HALF_UP) : BigDecimal.ZERO;
     }
 
-    public BigDecimal getTempoMedioOnibus() {
+    public BigDecimal calculaTempoMedioOnibus() {
         return Objects.nonNull(distanciaTotal) ?
                 distanciaTotal.divide(BigDecimal.valueOf(80), 2, RoundingMode.HALF_UP) : BigDecimal.ZERO;
     }
 
-    public BigDecimal getTempoMedioCaminhao() {
+    public BigDecimal calculaTempoMedioCaminhao() {
         return Objects.nonNull(distanciaTotal) ?
                 distanciaTotal.divide(BigDecimal.valueOf(60), 2, RoundingMode.HALF_UP) : BigDecimal.ZERO;
     }
